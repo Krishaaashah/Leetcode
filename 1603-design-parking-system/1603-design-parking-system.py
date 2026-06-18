cd5261603db7609c3b97514eaadd193667292dc7
Krisha shah <krisha22102005@gmail.com>
2026-06-19 00:50:20 +0530

    Time: 39 ms (6.1%), Space: 12.8 MB (56.4%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.py
@@ -0,0 +1,27 @@
+class ParkingSystem(object):
+
+    def __init__(self, big, medium, small):
+        self.space = [0,big,medium,small]
+        """
+        :type big: int
+        :type medium: int
+        :type small: int
+        """
+        
+
+    def addCar(self, carType):
+        """
+        :type carType: int
+        :rtype: bool
+        """
+        if self.space[carType] > 0:
+            self.space[carType] -= 1
+            return True
+        else:
+            return False
+        
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
